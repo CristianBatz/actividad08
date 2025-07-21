02e938702954a67988074ed71c6606fa28c4fc05
CristianBatz <ce.sbatz@udeo.edu.gt>
2025-07-21 11:14:38 -0600

    se a agregado las funciones de las primeras tres opciones sobre el factorial, la suma de numeros naturales y el fibonacci ademas de agregar las opciones en el menu

--- MenuMatematico.py
+++ MenuMatematico.py
@@ -1,4 +1,40 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+def numero_natural(n2):
+    if n2 == 1:
+        return 1
+    else:
+        return n2 + numero_natural(n2-1)
+def fibonacci(n3):
+    if n3 == 0:
+        return 0
+    else:
+        return n3 + fibonacci(n3-1)
+
 
 opcion = 0
-while opcion != 6:
+while opcion != 7:
     print("=== Menu Matematico ===")
+    print("1 - Factorial")
+    print("2 - Suma de numeros naturales")
+    print("3 - Fibonacci")
+    print("4 - buscar cantidad de letras en una palabra")
+    print("5 - cadena de texto")
+    print("6 - potencia de un numero")
+    print("7 - salir")
+    opcion = int(input("Seleccione una opcion: "))
+    if opcion == 1:
+        print("=== Factorial ===")
+        n = int(input("Ingresa un numero: "))
+        print(factorial(n))
+    if opcion == 2:
+        print("=== Suma de numeros naturales ===")
+        n2 = int(input("Ingresa un numero: "))
+        print(numero_natural(n2))
+    if opcion == 3:
+        print("=== Fibonacci ===")
+        n3 = int(input("Ingresa un numero: "))
+        print(fibonacci(n3))
