86b0778d03ac177c0ce9cfbb9fc327da96bf2a68
CristianBatz <ce.sbatz@udeo.edu.gt>
2025-07-21 19:44:20 -0600

    solucionando algunos problemas y agregando validaciones para los ingresos

--- MenuMatematico.py
+++ MenuMatematico.py
@@ -32,8 +32,6 @@ def invertir_texto(texto, i=0):
 def potencia(base, exponente):
     if exponente == 0:
         return 1
-    elif exponente == 1:
-        return base
     else:
         return base * potencia(base, exponente - 1)
 
@@ -47,33 +45,73 @@ while opcion != 7:
     print("5 - cadena de texto")
     print("6 - potencia de un numero")
     print("7 - salir")
-    opcion = int(input("Seleccione una opcion: "))
+    try:
+        opcion = int(input("Seleccione una opcion: "))
+    except ValueError:
+        print("Opcion no valida")
+        continue
+
     if opcion == 1:
         print("=== Factorial ===")
-        n = int(input("Ingresa un numero: "))
-        print(factorial(n))
+        try:
+            n = int(input("Ingresa un numero: "))
+            if n<0:
+                print("Ha ingresado un numero negativo")
+            else:
+                print("Resultado:", factorial(n))
+        except ValueError:
+            print("Error: Debe ingresar un número entero.")
+
     elif opcion == 2:
         print("=== Suma de numeros naturales ===")
-        n2 = int(input("Ingresa un numero: "))
-        print(numero_natural(n2))
+        try:
+            n2 = int(input("Ingresa un numero: "))
+            if n2 <= 0:
+                print(" El número debe ser mayor que cero.")
+            else:
+                print("Resultado:", numero_natural(n2))
+        except ValueError:
+            print("Error: Ingrese un número válido.")
+
     elif opcion == 3:
         print("=== Fibonacci ===")
-        n3 = int(input("Ingresa un numero: "))
-        print(fibonacci(n3))
+        try:
+            n3 = int(input("Ingresa un numero: "))
+            if n3 < 0:
+                print("El número debe ser positivo.")
+            else:
+                print("Resultado:", fibonacci(n3))
+        except ValueError:
+            print("Error: Ingrese un número válido.")
+
     elif opcion == 4:
         print("=== buscar letras en una palabra ===")
         n4 = input("Ingresa una palabra: ")
         n5 = input("Ingresa una letra: ")
-        print(cantidad_letras(n4, n5))
+        if len(n5) != 1:
+            print("Error: Debe ingresar solo una letra.")
+        else:
+            print("La letra aparece:", cantidad_letras(n4, n5), "veces")
+
     elif opcion == 5:
         print("=== Cadena de texto ===")
         texto = input("Ingrese un texto: ")
-        print(invertir_texto(texto))
+        print(f"Texto invertido: {invertir_texto(texto)}")
+
     elif opcion == 6:
         print("=== potencia de un numero ===")
-        base = int(input("Ingresa un numero: "))
-        exponente = int(input("Ingresa un numero: "))
-        print(potencia(base, exponente))
-    if opcion == 7:
-        print("=== Salir ===")
+        try:
+            base = int(input("Ingresa un numero: "))
+            exponente = int(input("Ingresa un numero: "))
+            if exponente < 0:
+                print("El exponente debe ser positivo.")
+            else:
+                print("Resultado:", potencia(base, exponente))
+        except ValueError:
+            print("Error: Ingrese solo números enteros.")
+
+    elif opcion == 7:
         print("Saliendo")
+
+    else:
+        print("Opción no válida.")
